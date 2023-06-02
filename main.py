0d2d0146617326e26f12b40c173c7c89ffd41c1d
Sviatoslav <73437401+SviatoslavT@users.noreply.github.com>
2023-06-02 03:32:38 +0300

    some changes

--- main.py
+++ main.py
@@ -8,34 +8,38 @@ from BlackWhite2Color import process_image
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
-# Telegram bot token (replace with your own)
+
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+help_message = """
+👋 Hi! I'm a bot that can colorize your black and white photos!
+
+Send me a photo and I'll colorize it!
+
+Made by @sviat_dev
+"""
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply(help_message)
 
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
         image = message.photo[-1].file_id
         file_path = await bot.get_file(image)
         downloaded_file = await bot.download_file(file_path.file_path)
-        process_image(downloaded_file)
-
-        await bot.send_photo(message.chat.id, photo=open('orig_after.png', 'rb'))
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
+        message_id = await bot.send_message(message.chat.id, '⏳ Processing image...')
+        img = await process_image(downloaded_file)
+        await bot.delete_message(message.chat.id, message_id.message_id)
+        result = types.InputFile(img, filename='result.png')
+        await bot.send_photo(message.chat.id, photo=result)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
